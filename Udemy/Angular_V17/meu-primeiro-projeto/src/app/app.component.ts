063a007908b8195bed17d5f4b07a80aef9cf68fd
Paulo Santos <phs.developer@gmail.com>
2024-10-18 20:01:59 -0300

    Aula 015. Criando um componente >> Aula finalziada e compilando com sucesso.

--- Udemy/Angular_V17/meu-primeiro-projeto/src/app/app.component.ts
+++ Udemy/Angular_V17/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NewComponent],
   template: `
   <!--<router-outlet /> -->
-  <h1>Curso de Angular</h1>
-  <h2>Components</h2>
+  <app-new-component/>
+
   `
 })
 export class AppComponent {
