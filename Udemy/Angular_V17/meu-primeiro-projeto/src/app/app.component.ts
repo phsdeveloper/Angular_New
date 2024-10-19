abc237803068a18b202e0f07e15aaebe69f0d843
Paulo Santos <phs.developer@gmail.com>
2024-10-19 18:47:29 -0300

    Aula 022. Text Interpolation >> Aula finalizada com sucesso e código compilando,

--- Udemy/Angular_V17/meu-primeiro-projeto/src/app/app.component.ts
+++ Udemy/Angular_V17/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet,NewComponent],
+  imports: [RouterOutlet,NewComponent,TemplateBindingComponent],
   template: `
   <!--<router-outlet /> -->
   <app-new-component/>
+  <app-template-binding />
 
   `
 })
---
+++ Udemy/Angular_V17/meu-primeiro-projeto/src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+  public name = 'Paulo Santos';
+  public age = 42;
+
+  public sum(val1: number, val2:number){
+    return val1 + val2;
+  }
+}
