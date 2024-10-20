b07ea85dda9d6ce02132f50c5ab2f8a6970d02d5
Paulo Santos <phs.developer@gmail.com>
2024-10-20 18:23:48 -0300

    Aula 023. Property binding >> Finalizada com sucesso e código compilando.

--- Udemy/Angular_V17/meu-primeiro-projeto/src/app/components/template/template-binding/template-binding.component.ts
+++ Udemy/Angular_V17/meu-primeiro-projeto/src/app/components/template/template-binding/template-binding.component.ts
@@ -8,13 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
+
+constructor(){
+  
+}
+
   public CaminhoComponente = 'src/app/components/template/template-binding'
 
   public name = 'Paulo Santos';
   public age = 42;
   public descricao = 'O Text interpolation é uma das formas de comunicação entre o "Back-end" com o "Front-end" do Angular, onde essa comunicação é realizada através de dois pares de chaves {{ valor }}';
-  
-  public sum(val1: number, val2: number){
-  return val1 + val2;
-}
+  public sum(val1: number, val2: number) {
+    return val1 + val2;
+  }
+
+  /**************************************************************************************************************************
+   *                                        23. Property binding                                                            *
+  ***************************************************************************************************************************/
+
+  public descricao2 = 'O Property Binding permite que a partir do "Back end" do Angular possamos controlar elementos HTML no "Front end" do angular. Isso é possóvel através do seguinte comando <element [property]="value"></element>';
+  public isDisabled = true;
+  public srcValue = 'https://res-1.cdn.office.net/files/fabric-cdn-prod_20240610.001/assets/item-types/32/folder.svg';
+  public imgAlt = 'Property binding IMG ALT';
+  public imgTitle = 'Property binding IMG Title';
+
+
 }
